48b546cb5ff87dc338b2c39426376d16045ac165
tangpeiqi <peiqitang@gmail.com>
2026-02-11 14:39:25 -0800

    basic permission

---
+++ Draft1/Draft1PhysicalBtn/Draft1PhysicalBtn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Draft1PhysicalBtn
+//
+//  Created by Peiqi Tang on 2/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
